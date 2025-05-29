32e0fb8a9d6a0e520038d407b3ee5c96065e9134
Fazal0567 <fazalalicontribute@gmail.com>
2025-05-30 01:43:53 +0530

    frontend complete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes.js');
+const cors = require('cors');
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/',(req,res)=>{
     res.send('Hello World!');
